a96e8e0ae99f19071c18133a8bbd426ad023173e
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-08-01 14:35:59 -0300

    cambios en tamaños de cards

--- src/components/views/Conocimientos.jsx
+++ src/components/views/Conocimientos.jsx
@@ -9,55 +9,55 @@ const Conocimientos = () => {
         <Container className='py-4' id="conocimientos">
             <h3 className='text-center text-light titulo mb-3'>Conocimientos</h3>
             <Row className='text-light d-flex justify-content-center'>
-                <Col xs={8} md={4} className='text-center'>
+                <Col xs={10} md={4} className='text-center'>
                     <article className='tarjetaConocimiento rounded bg-secondary my-3 p-2'>
                         <FaHtml5 className='icono'></FaHtml5>
                         <h4>HTML</h4>
                     </article>
                 </Col>
-                <Col xs={8} md={4} className='text-center'>
+                <Col xs={10} md={4} className='text-center'>
                     <article className='tarjetaConocimiento rounded bg-secondary my-3 p-2'>
                         <FaCss3Alt className='icono'></FaCss3Alt>
                         <h4>CSS</h4>
                     </article>
                 </Col>
-                <Col xs={8} md={4} className='text-center'>
+                <Col xs={10} md={4} className='text-center'>
                     <article className='tarjetaConocimiento rounded bg-secondary my-3 p-2'>
                         <DiJavascript className='icono'></DiJavascript>
                         <h4>JAVASCRIPT</h4>
                     </article>
                 </Col>
-                <Col xs={8} md={4} className='text-center'>
+                <Col xs={10} md={4} className='text-center'>
                     <article className='tarjetaConocimiento rounded bg-secondary my-3 p-2'>
                         <FaReact className='icono'></FaReact>
                         <h4>REACT</h4>
                     </article>
                 </Col>
-                <Col xs={8} md={4} className='text-center'>
+                <Col xs={10} md={4} className='text-center'>
                     <article className='tarjetaConocimiento rounded bg-secondary my-3 p-2'>
                         <FaNodeJs className='icono'></FaNodeJs>
                         <h4>NODE JS</h4>
                     </article>
                 </Col>
-                <Col xs={8} md={4} className='text-center'>
+                <Col xs={10} md={4} className='text-center'>
                     <article className='tarjetaConocimiento rounded bg-secondary my-3 p-2'>
                         <DiMongodb className='icono'></DiMongodb>
                         <h4>MONGO DB</h4>
                     </article>
                 </Col>
-                <Col xs={8} md={4} className='text-center'>
+                <Col xs={10} md={4} className='text-center'>
                     <article className='tarjetaConocimiento rounded bg-secondary my-3 p-2'>
                         <DiMysql className='icono'></DiMysql>
                         <h4>MYSQL</h4>
                     </article>
                 </Col>
-                <Col xs={8} md={4} className='text-center'>
+                <Col xs={10} md={4} className='text-center'>
                     <article className='tarjetaConocimiento rounded bg-secondary my-3 p-2'>
                         <FaGithub className='icono'></FaGithub>
                         <h4>GITHUB</h4>
                     </article>
                 </Col>
-                <Col xs={8} md={4} className='text-center'>
+                <Col xs={10} md={4} className='text-center'>
                     <article className='tarjetaConocimiento rounded bg-secondary my-3 p-2'>
                         <FaBootstrap className='icono'></FaBootstrap>
                         <h4>Bootstrap</h4>
--- src/components/views/Proyectos.jsx
+++ src/components/views/Proyectos.jsx
@@ -8,26 +8,26 @@ const Proyectos = () => {
         <Container className='py-4 text-center' id="proyectos">
             <h3 className='text-center text-light titulo mb-3'>Proyectos</h3>
             <Row className='text-light d-flex justify-content-center'>
-                <Col xs={8} md={4} className='text-center'>
+                <Col xs={11} md={4} className='text-center'>
                     <article className='rounded bg-secondary m-3 p-2 cardProyectos'>
                         <img src={portadaPortafolio} alt=" imagen proyecto portafolio" width="100%" className='rounded' />
                         <h4 className='mt-2'>Mi portafolio</h4>
                         <a href="https://github.com/joaquin-fuentes/nuevoportafolio" target='_blank' className='linkProyectoGithub'>Ver Repositorio de Github</a>
                     </article>
                 </Col>
-                <Col xs={8} md={4} className='text-center'>
+                <Col xs={11} md={4} className='text-center'>
                 <article className='rounded bg-secondary m-3 p-2 cardProyectos'>
                         <img src={portadaRecetas} alt=" imagen proyecto recetas de cocina" width="100%" className='rounded' />
                         <h4 className='mt-2'>Recetas de cocina</h4>
                         <a href="https://github.com/joaquin-fuentes/recetascliente" target='_blank' className='linkProyectoGithub'>Ver Repositorio de Github</a>
                     </article>
                 </Col>
-                <Col xs={8} md={4} className='text-center'>
+                <Col xs={11} md={4} className='text-center'>
                     <article className='rounded bg-secondary m-3 p-2 cardProyectos'>
                         <h4>PRoyecto 3</h4>
                     </article>
                 </Col>
-                <Col xs={8} md={4} className='text-center'>
+                <Col xs={11} md={4} className='text-center'>
                     <article className='rounded bg-secondary m-3 p-2 cardProyectos'>
                         <h4>PRoyecto 4</h4>
                     </article>
