7cd2ccb234d196ea983727cb751491e1e82a8844
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-08-03 12:08:31 -0300

    arregle medidas para tablets

--- src/components/views/Proyectos.jsx
+++ src/components/views/Proyectos.jsx
@@ -9,21 +9,21 @@ const Proyectos = () => {
         <Container className='py-4 text-center' id="proyectos">
             <h3 className='text-center text-light titulo mb-3'>Proyectos</h3>
             <Row className='text-light d-flex justify-content-center'>
-                <Col xs={11} md={4} className='text-center'>
+                <Col xs={11} md={5} lg={4} className='text-center'>
                     <article className='rounded bg-secondary m-3 p-2 cardProyectos'>
                         <img src={portadaPortafolio} alt=" imagen proyecto portafolio" width="100%" className='rounded' />
                         <h4 className='mt-2'>Mi portafolio</h4>
                         <a href="https://github.com/joaquin-fuentes/nuevoportafolio" target='_blank' className='linkProyectoGithub'>Ver Repositorio de Github</a>
                     </article>
                 </Col>
-                <Col xs={11} md={4} className='text-center'>
+                <Col xs={11} md={5} lg={4} className='text-center'>
                     <article className='rounded bg-secondary m-3 p-2 cardProyectos'>
                         <img src={portadaRecetas} alt=" imagen proyecto recetas de cocina" width="100%" className='rounded' />
                         <h4 className='mt-2'>Recetas de cocina</h4>
                         <a href="https://github.com/joaquin-fuentes/recetascliente" target='_blank' className='linkProyectoGithub'>Ver Repositorio de Github</a>
                     </article>
                 </Col>
-                <Col xs={11} md={4} className='text-center'>
+                <Col xs={11} md={5} lg={4} className='text-center'>
                     <article className='rounded bg-secondary m-3 p-2 cardProyectos'>
                         <img src={portadaTiendaAnime} alt=" imagen proyecto tienda anime" width="100%" className='rounded' />
                         <h4 className='mt-2'>Tienda de anime</h4>
