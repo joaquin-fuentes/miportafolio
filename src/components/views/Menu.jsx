ee5edaa8faa3b90d6e2a92e0ceee38c74ac90d8c
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2024-04-19 10:35:31 -0300

    cambie imagen de fondo de portada y bold de navegacion

--- src/components/views/Menu.jsx
+++ src/components/views/Menu.jsx
@@ -11,12 +11,12 @@ const Menu = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" className='bg-secondary' />
                 <Navbar.Collapse id="basic-navbar-nav" >
                     <Nav className="ms-auto contenedorEnlacesNav">
-                        <Nav.Link className="text-light text-hover" href="#sobremi">Sobre mí</Nav.Link>
-                        <Nav.Link className="text-light text-hover" href="#experiencia">Experiencia</Nav.Link>
-                        <Nav.Link className="text-light text-hover" href="#conocimientos">Conocimientos</Nav.Link>
-                        <Nav.Link className="text-light text-hover" href="#contacto">Contacto</Nav.Link>
-                        <Nav.Link className="text-light text-hover" href="#educacion">Educacion</Nav.Link>
-                        <Nav.Link className="text-light text-hover" href="#proyectos">Proyectos</Nav.Link>
+                        <Nav.Link className="text-light text-hover fw-bold" href="#sobremi">Sobre mí</Nav.Link>
+                        <Nav.Link className="text-light text-hover fw-bold" href="#experiencia">Experiencia</Nav.Link>
+                        <Nav.Link className="text-light text-hover fw-bold" href="#conocimientos">Conocimientos</Nav.Link>
+                        <Nav.Link className="text-light text-hover fw-bold" href="#contacto">Contacto</Nav.Link>
+                        <Nav.Link className="text-light text-hover fw-bold" href="#educacion">Educacion</Nav.Link>
+                        <Nav.Link className="text-light text-hover fw-bold" href="#proyectos">Proyectos</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
