d40ff2ab6863e629d77fab05ddecf1f58dd1ac36
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2024-07-30 15:55:34 -0300

    cambie tamaño de columna de conocimientos

--- src/components/views/Contacto.jsx
+++ src/components/views/Contacto.jsx
@@ -9,10 +9,10 @@ const Contacto = () => {
     return (
         <Container data-aos="fade-up" className='text-light p-4 text-center d-flex flex-column' id="contacto" >
             <h2 className=' titulo mb-4'>Contacto</h2>
-            <a href={url} target='_blank' className='datoContacto'> <FaWhatsapp className='fs-3 mx-1 text-success'></FaWhatsapp>381-6097754</a>
-            <h5 className='datoContacto'>< FiMail className='fs-3 mx-1 text-danger'></FiMail>JOAQUIN.FUENTES1327@GMAIL.COM</h5>
-            <a href='https://www.linkedin.com/in/joaqu%C3%ADn-fuentes/' target='_blank' className=' datoContacto '> <FaLinkedin className='fs-3 mx-1 text-primary'></FaLinkedin>Perfil de LINKEDIN</a>
-            <a href='https://github.com/joaquin-fuentes' target='_blank' className=' datoContacto '> <FaGithub className='fs-3 mx-1 text-black bg-white rounded-circle'></FaGithub>Perfil de GITHUB</a>
+            <a href={url} target='_blank' className='datoContacto'> <FaWhatsapp className='fs-3 mx-1'></FaWhatsapp>381-6097754</a>
+            <h5 className='datoContacto'>< FiMail className='fs-3 mx-1'></FiMail>JOAQUIN.FUENTES1327@GMAIL.COM</h5>
+            <a href='https://www.linkedin.com/in/joaqu%C3%ADn-fuentes/' target='_blank' className=' datoContacto '> <FaLinkedin className='fs-3 mx-1'></FaLinkedin>Perfil de LINKEDIN</a>
+            <a href='https://github.com/joaquin-fuentes' target='_blank' className=' datoContacto '> <FaGithub className='fs-3 mx-1'></FaGithub>Perfil de GITHUB</a>
         </Container>
     );
 };
